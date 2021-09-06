d5615243352d800e302bcf403e9da944526a5535
Ariadne Bigheti <83436908+AriadneBigheti@users.noreply.github.com>
2021-09-06 14:02:52 -0300

    Update sdl.cpp

--- sdl.cpp
+++ sdl.cpp
@@ -15,30 +15,8 @@ class SystemModel{
     SDL_Rect target;
     
   public:
-    SystemModel(){
-            // Criando uma janela
-      window = nullptr;
-      window = SDL_CreateWindow("Demonstracao do SDL2",
-          SDL_WINDOWPOS_UNDEFINED,
-          SDL_WINDOWPOS_UNDEFINED,
-          SCREEN_WIDTH,
-          SCREEN_HEIGHT,
-          SDL_WINDOW_SHOWN);
-      if (window==nullptr) { // Em caso de erro...
-        std::cout << SDL_GetError();
-        SDL_Quit();
-        return 1;
-      }
-        // Inicializando o renderizador
-    renderer = SDL_CreateRenderer(
-        window, -1,
-        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    if (renderer==nullptr) { // Em caso de erro...
-      SDL_DestroyWindow(window);
-      std::cout << SDL_GetError();
-      SDL_Quit();
-      return 1;
-    }
+    SystemModel(SDL_Renderer* renderer, SDL_Window* window) : renderer(renderer), window(window) {
+
       // Carregando texturas
       // personagem
       texture = IMG_LoadTexture(renderer, "./capi.png");
@@ -51,27 +29,29 @@ class SystemModel{
       SDL_QueryTexture(texture, nullptr, nullptr, &target.w, &target.h);
           
     }
-    void buildWindow(){
-
-    }
+   
     SDL_Renderer* get_renderer(){
-      return this->renderer
+      return this->renderer;
     }
     SDL_Window* get_window(){
       return this->window;
     }
-    SDL_Texture get_texture(){
+    SDL_Texture* get_texture(){
       return this->texture;
     }
-    SDL_Texture get_texture2(){
+    SDL_Texture* get_texture2(){
       return this->texture2;
     }
-    SDL_Rect get_target(){
-      return this->target;
+    SDL_Rect* get_target(){
+      return &this->target;
+    }
+    void set_target(int x, int y){
+      this->target.x += x;
+      this->target.y += y;
     }
 
 
-}
+};
 
 //View
 class SystemView{
@@ -85,7 +65,7 @@ class SystemView{
         // Desenhar a cena
       SDL_RenderClear(model.get_renderer());
       SDL_RenderCopy(model.get_renderer(), model.get_texture2(), nullptr, nullptr);
-      SDL_RenderCopy(model.get_renderer(), model.get_texture(), nullptr, &model.get_target());
+      SDL_RenderCopy(model.get_renderer(), model.get_texture(), nullptr, (model).get_target());
       SDL_RenderPresent(model.get_renderer());
 
       // Delay para diminuir o framerate
@@ -105,40 +85,74 @@ class SystemView{
 //Controller
 class SystemController{
   private:
-    bool rodando = true;
+    bool rodando;
     // Variaveis para verificar eventos
     SDL_Event evento; // eventos discretos
-    const Uint8* state = SDL_GetKeyboardState(nullptr); // estado do teclado
+    const Uint8* state;  // estado do teclado
+
+  SystemModel &model;
   
   public:
   // Polling de eventos
-    SDL_PumpEvents(); // atualiza estado do teclado
-    if (state[SDL_SCANCODE_LEFT]) target.x -= 1;
-    if (state[SDL_SCANCODE_RIGHT]) target.x += 1;
-    if (state[SDL_SCANCODE_UP]) target.y -= 1;
-    if (state[SDL_SCANCODE_DOWN]) target.y += 1;
 
-
-    while (SDL_PollEvent(&evento)) {
-      if (evento.type == SDL_QUIT) {
-        this->rodando = false;
-      }
-      // Exemplos de outros eventos.
-      // Que outros eventos poderiamos ter e que sao uteis?
-      //if (evento.type == SDL_KEYDOWN) {
-      //}
-      //if (evento.type == SDL_MOUSEBUTTONDOWN) {
-      //}
+    SystemController(SystemModel &model) : model(model){
+        this->rodando = true;
+        this->state = SDL_GetKeyboardState(nullptr);
     }
 
-}
+    void polling(){
+      SDL_PumpEvents(); // atualiza estado do teclado
+      if (state[SDL_SCANCODE_LEFT]) model.set_target((-1)*1, 0);
+      if (state[SDL_SCANCODE_RIGHT]) model.set_target(1, 0);
+      if (state[SDL_SCANCODE_UP]) model.set_target(0, (-1)*1);
+      if (state[SDL_SCANCODE_DOWN]) model.set_target(0, 1);
 
 
+      while (SDL_PollEvent(&evento)) {
+        if (evento.type == SDL_QUIT) {
+          this->rodando = false;
+        }
+        // Exemplos de outros eventos.
+        // Que outros eventos poderiamos ter e que sao uteis?
+        //if (evento.type == SDL_KEYDOWN) {
+        //}
+        //if (evento.type == SDL_MOUSEBUTTONDOWN) {
+        //}
+      }
+    }  
+    bool get_rodando(){
+      return get_rodando();
+    }
+    
+};
 
 int main() {
-  SystemModel model = SystemModel();
-  SystemView view = SystemView(model);
-  SystemController controller = SystemController();
+  SDL_Renderer* renderer;
+  SDL_Window* window;
+
+  // Criando uma janela
+  window = nullptr;
+  window = SDL_CreateWindow("Demonstracao do SDL2",
+    SDL_WINDOWPOS_UNDEFINED,
+    SDL_WINDOWPOS_UNDEFINED,
+    SCREEN_WIDTH,
+    SCREEN_HEIGHT,
+    SDL_WINDOW_SHOWN);
+  if (window==nullptr) { // Em caso de erro...
+    std::cout << SDL_GetError();
+    SDL_Quit();
+    return 1;
+  }
+    // Inicializando o renderizador
+  renderer = SDL_CreateRenderer(
+    window, -1,
+    SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+  if (renderer==nullptr) { // Em caso de erro...
+    SDL_DestroyWindow(window);
+    std::cout << SDL_GetError();
+    SDL_Quit();
+    return 1;
+  }
 
   // Inicializando o subsistema de video do SDL
   if ( SDL_Init (SDL_INIT_VIDEO) < 0 ) {
@@ -146,12 +160,19 @@ int main() {
     return 1;
   }
 
+
+  SystemModel model = SystemModel(renderer, window);
+  SystemView view = SystemView(model);
+  SystemController controller = SystemController(model);
+
+
   // Laco principal do jogo
   while(controller.get_rodando()) {
-    
-    //renderizar()
+    controller.polling();
+    view.renderizar();
   }
 
-  //destruir()
+  view.destruir();
+  
   return 0;
 }
